15ff42b7999e8d60caee35083abe511498b3e87f
Barthmosz <126964783+Barthmosz@users.noreply.github.com>
2023-05-01 00:56:49 -0300

    refactor: move sut creation to a factory helper method

--- src/cep-validator.spec.ts
+++ src/cep-validator.spec.ts
@@ -1,18 +1,27 @@
 import { CepValidator } from './cep-validator'
 
+interface SutTypes {
+    sut: CepValidator
+}
+
+const makeSut = (): SutTypes => {
+    const sut = new CepValidator()
+    return { sut }
+}
+
 describe('Cep Validator', () => {
     test('should return false if an invalid cep is provided', () => {
-        const sut = new CepValidator()
+        const { sut } = makeSut()
         expect(sut.validate('12345')).toBe(false)
     })
 
     test('should return false if an empty cep is provided', () => {
-        const sut = new CepValidator()
+        const { sut } = makeSut()
         expect(sut.validate('')).toBe(false)
     })
 
     test('should return true if a valid cep is provided', () => {
-        const sut = new CepValidator()
+        const { sut } = makeSut()
         expect(sut.validate('12345-678')).toBe(true)
     })
 })
