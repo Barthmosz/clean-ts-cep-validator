f9dffa27fa1b9baa2ae5ebb76c67f1d693f4d019
Barthmosz <126964783+Barthmosz@users.noreply.github.com>
2023-05-01 00:58:46 -0300

    chore: add jsdocs to CepValidator

--- src/cep-validator.ts
+++ src/cep-validator.ts
@@ -1,4 +1,9 @@
 export class CepValidator {
+    /**
+     * Validates a cep.
+     * @param {string} cep
+     * @returns {boolean}
+    */
     validate(cep: string): boolean {
         const cepRegex = /^[0-9]{5}-[0-9]{3}$/
         return cepRegex.test(cep)
