3b2e65b94ff0130bf71df30eb1e9acf2e528c4fe
Barthmosz <126964783+Barthmosz@users.noreply.github.com>
2023-05-01 00:55:07 -0300

    test: ensure CepValidator returns true if a valid cep is provided

--- src/cep-validator.spec.ts
+++ src/cep-validator.spec.ts
@@ -10,4 +10,9 @@ describe('Cep Validator', () => {
         const sut = new CepValidator()
         expect(sut.validate('')).toBe(false)
     })
+
+    test('should return true if a valid cep is provided', () => {
+        const sut = new CepValidator()
+        expect(sut.validate('12345-678')).toBe(true)
+    })
 })
